781addd7b3185f654f98a3166b13cb6cc4e2ef63
chathura <chathuraperera007@gmail.com>
2021-08-28 23:53:56 +0530

    Initial Commit

---
+++ pages/about.js
@@ -0,0 +1,23 @@
+import React from "react";
+import Footer from "../components/Footer";
+import Navbar from "../components/Navbar";
+
+const about = () => {
+  return (
+    <div>
+      <h1>About</h1>
+      <p>
+        Lorem ipsum dolor sit amet consectetur, adipisicing elit. Debitis illum
+        earum eius laudantium esse, nam fugiat alias reprehenderit odio
+        doloribus?
+      </p>
+      <p>
+        Lorem ipsum dolor sit amet consectetur, adipisicing elit. Debitis illum
+        earum eius laudantium esse, nam fugiat alias reprehenderit odio
+        doloribus?
+      </p>
+    </div>
+  );
+};
+
+export default about;
